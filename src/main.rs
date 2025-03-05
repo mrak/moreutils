324449ed3ac983dd573d3bdebfd4ca9acc1d1c9e
Eric Mrak <enmrak@gmail.com>
2025-03-04 22:47:29 -0800

    base funcationality

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,10 @@
 use std::env;
+use std::fs;
+use std::fs::OpenOptions;
+use std::io;
+use std::io::prelude::*;
+use std::io::BufWriter;
+use std::path::PathBuf;
 use std::process::exit;
 
 struct Options {
@@ -10,7 +16,7 @@ fn usage() {
     println!("sponge [-a] FILE");
 }
 
-fn main() {
+fn main() -> io::Result<()> {
     let args: Vec<String> = env::args().skip(1).collect();
     let options = match args.len() {
         2 => Options {
@@ -26,5 +32,40 @@ fn main() {
             exit(1)
         }
     };
-    println!("append: {}, file: {}", options.append, options.file);
+
+    let tmpfilename = soak(&options)?;
+
+    fs::copy(&tmpfilename, options.file)?;
+    fs::remove_file(&tmpfilename)?;
+
+    Ok(())
+}
+
+fn soak(options: &Options) -> io::Result<PathBuf> {
+    let stdin = io::stdin();
+    let mut stdin = stdin.lock();
+
+    let tmpfile = env::temp_dir().join(format!("sponge.{}", std::process::id()));
+
+    if options.append {
+        fs::copy(&options.file, &tmpfile)?;
+    }
+    let file = OpenOptions::new()
+        .create(true)
+        .append(true)
+        .open(&tmpfile)?;
+    let mut writer = BufWriter::new(file);
+
+    loop {
+        let buffer = stdin.fill_buf()?;
+        let buflen = buffer.len();
+        if buffer.is_empty() {
+            break;
+        }
+        writer.write_all(buffer)?;
+        stdin.consume(buflen);
+    }
+
+    writer.flush()?;
+    Ok(tmpfile)
 }
