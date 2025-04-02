faf82dc7ae82b469d782fbe0552e4acff0d8b347
Eric Mrak <enmrak@gmail.com>
2025-04-01 21:04:17 -0700

    switch to in-memory buffer

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,14 @@
 use std::env;
-use std::fs;
 use std::fs::OpenOptions;
 use std::io;
-use std::io::BufRead;
-use std::io::BufWriter;
+use std::io::Read;
 use std::io::Write;
-use std::os::unix::fs::PermissionsExt;
 use std::path::Path;
-use std::path::PathBuf;
 use std::process::exit;
 
 struct Options<'a> {
     append: bool,
     file: &'a Path,
-}
-
-fn usage() {
-    println!("sponge [-a] FILE");
 }
 
 fn main() -> io::Result<()> {
@@ -31,60 +23,22 @@ fn main() -> io::Result<()> {
             file: Path::new(&args[0]),
         },
         _ => {
-            usage();
+            println!("sponge [-a] FILE");
             exit(1)
         }
     };
 
-    let tmpfilename = soak(&options)?;
-    squeeze(&tmpfilename, options.file)?;
-
-    Ok(())
-}
-
-fn squeeze(tmpfilename: &PathBuf, target: &Path) -> io::Result<()> {
-    let original_mode = match fs::metadata(target) {
-        Ok(meta) => Some(meta.permissions().mode()),
-        Err(_) => None,
-    };
-
-    if fs::rename(tmpfilename, target).is_err() {
-        fs::copy(tmpfilename, target)?;
-        fs::remove_file(tmpfilename)?;
-    }
-
-    if let Some(mode) = original_mode {
-        let mut permissions = fs::metadata(target)?.permissions();
-        permissions.set_mode(mode);
-    }
-    Ok(())
-}
-
-fn soak(options: &Options) -> io::Result<PathBuf> {
     let stdin = io::stdin();
     let mut stdin = stdin.lock();
+    let mut buffer = Vec::new();
+    stdin.read_to_end(&mut buffer)?;
 
-    let tmpfile = env::temp_dir().join(format!("sponge.{}", std::process::id()));
-
-    if options.append {
-        fs::copy(options.file, &tmpfile)?;
-    }
-    let file = OpenOptions::new()
+    OpenOptions::new()
         .create(true)
-        .append(true)
-        .open(&tmpfile)?;
-    let mut writer = BufWriter::new(file);
+        .append(options.append)
+        .write(true)
+        .open(options.file)?
+        .write_all(&buffer)?;
 
-    loop {
-        let buffer = stdin.fill_buf()?;
-        let buflen = buffer.len();
-        if buffer.is_empty() {
-            break;
-        }
-        writer.write_all(buffer)?;
-        stdin.consume(buflen);
-    }
-
-    writer.flush()?;
-    Ok(tmpfile)
+    Ok(())
 }
