2d96eaa8d63557d7c62ddb202b95f04a47d32bdf
Eric Mrak <enmrak@gmail.com>
2025-04-01 22:09:28 -0700

    multibinary

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,23 @@
+use moarutils::sponge;
 use std::env;
-use std::fs::OpenOptions;
+use std::ffi::OsStr;
 use std::io;
-use std::io::Read;
-use std::io::Write;
 use std::path::Path;
-use std::process::exit;
-
-struct Options<'a> {
-    append: bool,
-    file: &'a Path,
-}
 
 fn main() -> io::Result<()> {
-    let args: Vec<String> = env::args().skip(1).collect();
-    let options = match args.len() {
-        2 => Options {
-            append: args[0].eq("-a"),
-            file: Path::new(&args[1]),
-        },
-        1 => Options {
-            append: false,
-            file: Path::new(&args[0]),
-        },
-        _ => {
-            println!("sponge [-a] FILE");
-            exit(1)
+    let arg0 = env::args().next();
+    let cmd = arg0
+        .as_ref()
+        .map(Path::new)
+        .and_then(Path::file_name)
+        .and_then(OsStr::to_str)
+        .unwrap();
+    match cmd {
+        "sponge" => sponge::sponge(),
+        "vipe" => {
+            println!("vipe");
+            Ok(())
         }
-    };
-
-    let stdin = io::stdin();
-    let mut stdin = stdin.lock();
-    let mut buffer = Vec::new();
-    stdin.read_to_end(&mut buffer)?;
-
-    OpenOptions::new()
-        .create(true)
-        .append(options.append)
-        .write(true)
-        .open(options.file)?
-        .write_all(&buffer)?;
-
-    Ok(())
+        x => panic!("not implemented: {}", x),
+    }
 }
---
+++ src/sponge.rs
@@ -0,0 +1,44 @@
+use std::env;
+use std::fs::OpenOptions;
+use std::io;
+use std::io::Read;
+use std::io::Write;
+use std::path::Path;
+use std::process::exit;
+
+struct Options<'a> {
+    append: bool,
+    file: &'a Path,
+}
+
+pub fn sponge() -> io::Result<()> {
+    let args: Vec<String> = env::args().skip(1).collect();
+    let options = match args.len() {
+        2 => Options {
+            append: args[0].eq("-a"),
+            file: Path::new(&args[1]),
+        },
+        1 => Options {
+            append: false,
+            file: Path::new(&args[0]),
+        },
+        _ => {
+            println!("sponge [-a] FILE");
+            exit(1)
+        }
+    };
+
+    let stdin = io::stdin();
+    let mut stdin = stdin.lock();
+    let mut buffer = Vec::new();
+    stdin.read_to_end(&mut buffer)?;
+
+    OpenOptions::new()
+        .create(true)
+        .append(options.append)
+        .write(true)
+        .open(options.file)?
+        .write_all(&buffer)?;
+
+    Ok(())
+}
