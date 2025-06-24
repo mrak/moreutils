5fcd346fc6fccf5b006efafccb35ed4fd1e2c784
Eric Mrak <eric.mrak@fastly.com>
2025-06-24 15:48:47 -0700

    ifne: base case

--- src/ifne.rs
+++ src/ifne.rs
@@ -1,4 +1,10 @@
-use std::{env, io};
+use std::env;
+use std::io;
+use std::io::Read;
+use std::io::Write;
+use std::process::Command;
+use std::process::Stdio;
+use std::process::exit;
 
 fn usage() {
     eprintln!("Usage: ifne [-n] command");
@@ -11,8 +17,31 @@ pub fn ifne() -> io::Result<()> {
             let _ = args.next();
             true
         }
+        None => {
+            usage();
+            exit(1)
+        }
         _ => false,
     };
-    println!("invert is {}", invert);
+
+    let stdin = io::stdin();
+    let mut stdin = stdin.lock();
+    let mut peek_buffer = [0; 1];
+    let r = stdin.read(&mut peek_buffer);
+    let has_content = matches!(r, Ok(1));
+
+    if has_content {
+        let mut cmd = Command::new(args.next().unwrap())
+            .args(args)
+            .stdin(Stdio::piped())
+            .stdout(Stdio::inherit())
+            .stderr(Stdio::inherit())
+            .spawn()?;
+
+        let mut cmdstdin = cmd.stdin.take().expect("child stdin should be opened");
+        cmdstdin.write_all(&peek_buffer)?;
+        io::copy(&mut stdin, &mut cmdstdin)?;
+    }
+
     Ok(())
 }
