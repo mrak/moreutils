451775ce9e1236faf4e55296bc4d576840ee7b83
Eric Mrak <enmrak@gmail.com>
2025-07-01 21:05:52 -0700

    moreutils: usage

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,47 @@
 use std::env;
-use std::ffi::OsStr;
 use std::io;
 use std::path::Path;
 
+fn usage() {
+    eprintln!(
+        "
+moreutils is a multi-program binary that changes its operation based on the name it is
+invoked with. The following programs are included:
+
+  errno
+  ifne
+  pee
+  sponge
+  ts
+  vipe
+  vidir
+"
+    );
+}
+
 fn main() -> io::Result<()> {
-    let arg0 = env::args().next();
+    let arg0 = env::args_os().next();
     let cmd = arg0
-        .as_ref()
+        .as_deref()
         .map(Path::new)
         .and_then(Path::file_name)
-        .and_then(OsStr::to_str)
-        .expect("command name should be UTF-8 compliant");
-    match cmd {
-        "errno" => moreutils::errno(),
-        "ifne" => moreutils::ifne(),
-        "pee" => moreutils::pee(),
-        "sponge" => moreutils::sponge(),
-        "ts" => moreutils::ts(),
-        "vipe" => moreutils::vipe(),
-        "vidir" => moreutils::vidir(),
-        x => panic!("not implemented: {x}"),
+        .expect("always at least the program name");
+    match cmd.to_str() {
+        Some("errno") => moreutils::errno(),
+        Some("ifne") => moreutils::ifne(),
+        Some("pee") => moreutils::pee(),
+        Some("sponge") => moreutils::sponge(),
+        Some("ts") => moreutils::ts(),
+        Some("vipe") => moreutils::vipe(),
+        Some("vidir") => moreutils::vidir(),
+        Some("moreutils") => {
+            usage();
+            Ok(())
+        }
+        _ => {
+            usage();
+            eprintln!("not implemented: {cmd:?}");
+            Ok(())
+        }
     }
 }
