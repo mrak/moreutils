100f3686098bbbbbf34cb0a9d2af612a7895deb1
Eric Mrak <eric.mrak@fastly.com>
2025-07-09 12:07:00 -0700

    isutf8: match cleanups

--- src/isutf8.rs
+++ src/isutf8.rs
@@ -16,14 +16,14 @@ struct Options {
     files: Vec<OsString>,
 }
 
-enum Utf8Mode {
+enum Utf8 {
     Base,
-    TwoByte,
-    ThreeByteSecond(u8),
-    ThreeByteThird,
-    FourByteSecond(u8),
-    FourByteThird,
-    FourByteFourth,
+    Two,
+    Three(u8),
+    ThreeFinal,
+    Four(u8),
+    FourThird,
+    FourFinal,
 }
 
 fn usage() {}
@@ -85,32 +85,41 @@ pub fn isutf8() -> io::Result<()> {
     }
 
     for file in options.files {
-        let mut mode = Utf8Mode::Base;
-        let bytes = BufReader::new(File::open(&file)?).bytes();
-        for (count, byte) in bytes.enumerate() {
-            let byte = byte?;
-            match (&mode, byte) {
-                (Utf8Mode::Base, b'\x00'..=b'\x7F') => {}
-                (Utf8Mode::Base, b'\xC2'..=b'\xDF') => mode = Utf8Mode::TwoByte,
-                (Utf8Mode::TwoByte, b'\x80'..=b'\xBF') => mode = Utf8Mode::Base,
-                (Utf8Mode::ThreeByteSecond(b), b'\x80'..=b'\xBF') => match (b, byte) {
-                    (b'\xE0', b'\xA0'..=b'\xBF') => mode = Utf8Mode::ThreeByteThird,
-                    (b'\xE1'..=b'\xEC', b'\x80'..b'\xBF') => mode = Utf8Mode::ThreeByteThird,
-                    (b'\xED', b'\x80'..=b'\x9F') => mode = Utf8Mode::ThreeByteThird,
-                    (b'\xEE'..=b'\xEF', b'\x80'..=b'\x9F') => mode = Utf8Mode::ThreeByteThird,
-                    _ => unimplemented!(),
-                },
-                (Utf8Mode::ThreeByteThird, b'\x80'..b'\xBF') => mode = Utf8Mode::Base,
-                (Utf8Mode::FourByteSecond(b), b'\x80'..b'\xBF') => match (b, byte) {
-                    (b'\xF0', b'\x90'..b'\xBF') => mode = Utf8Mode::FourByteThird,
-                    _ => unimplemented!(),
-                },
-                (Utf8Mode::FourByteFourth, b'\x80'..b'\xBF') => mode = Utf8Mode::Base,
-                (Utf8Mode::FourByteFourth, b'\x80'..b'\xBF') => mode = Utf8Mode::Base,
-                // _ => unimplemented!(),
+        validate_file(&file)?;
+    }
+
+    Ok(())
+}
+
+fn validate_file(file: &OsString) -> io::Result<()> {
+    let mut mode = Utf8::Base;
+    let bytes = BufReader::new(File::open(file)?).bytes();
+    for (count, byte) in bytes.enumerate() {
+        let byte = byte?;
+        mode = match (&mode, byte) {
+            (Utf8::Base, b'\x00'..=b'\x7F') => Utf8::Base,
+            (Utf8::Base, b'\xC2'..=b'\xDF') => Utf8::Two,
+            (Utf8::Base, b'\xE0'..=b'\xEF') => Utf8::Three(byte),
+            (Utf8::Base, b'\xF0'..=b'\xF4') => Utf8::Four(byte),
+
+            (Utf8::Two, b'\x80'..=b'\xBF') => Utf8::Base,
+
+            (Utf8::Three(b'\xE0'), b'\xA0'..=b'\xBF') => Utf8::ThreeFinal,
+            (Utf8::Three(b'\xE1'..=b'\xEC'), b'\x80'..b'\xBF') => Utf8::ThreeFinal,
+            (Utf8::Three(b'\xED'), b'\x80'..=b'\x9F') => Utf8::ThreeFinal,
+            (Utf8::Three(b'\xEE'..=b'\xEF'), b'\x80'..=b'\x9F') => Utf8::ThreeFinal,
+            (Utf8::ThreeFinal, b'\x80'..=b'\xBF') => Utf8::Base,
+
+            (Utf8::Four(b'\xF0'), b'\x90'..b'\xBF') => Utf8::FourThird,
+            (Utf8::Four(b'\xF1'..=b'\xF3'), b'\x80'..b'\xBF') => Utf8::FourThird,
+            (Utf8::Four(b'\xF4'), b'\x80'..b'\x8F') => Utf8::FourThird,
+            (Utf8::FourThird, b'\x80'..=b'\xBF') => Utf8::FourFinal,
+            (Utf8::FourFinal, b'\x80'..=b'\xBF') => Utf8::Base,
+            _ => {
+                eprintln!("{file:?} is invalid UTF-8 at byte {count}");
+                break;
             }
         }
     }
-
     Ok(())
 }
